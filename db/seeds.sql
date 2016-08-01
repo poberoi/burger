c0e8ced074c5c82bb408f8ddef8ba80a894235eb
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-08-01 17:09:59 -0400

    DB setup completed

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('McDonald\'s Burger');
+INSERT INTO burgers (burger_name) VALUES ('White Castle Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wendy\'s Burger' , true);
