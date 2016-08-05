49e19ebba72ebeafdd547a142fb2a76b1baace0b
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-08-05 16:24:53 -0400

    orm functions completed

---
+++ config/orm.js
@@ -0,0 +1,64 @@
+// ==============================================================================
+// O.R.M functions
+// ==============================================================================
+
+var connection = require('./connection.js');
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push('?');
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  // column1=value, column2=value2,...
+  var arr = [];
+
+  for (var key in ob) {
+    if (ob.hasOwnProperty(key)) {
+      arr.push(key + '=' + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+var orm = {
+  selectAll: function(tableInput, cb){
+    var queryString = 'SELECT * FROM ' + tableInput + ';';
+    connection.query(queryString, function(err, result){
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb){
+    var queryString = 'INSERT INTO ' + table + ' (' + cols.toString() + ') VALUES (' + printQuestionMarks(vals.length) + ');';
+  
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result){
+      if (err) throw err;
+      cd(result);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = 'UPDATE ' + table;
+
+    queryString = queryString + ' SET ';
+    queryString = queryString + objToSql(objColVals);
+    queryString = queryString + ' WHERE ';
+    queryString = queryString + condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
